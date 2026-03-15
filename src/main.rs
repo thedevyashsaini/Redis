161e4e19c1e1b1e63db16b561b30bafb1283b915
Devyash Saini <dysaini2004@gmail.com>
2026-03-15 23:32:16 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,21 @@
 #![allow(unused_imports)]
-use std::{
-    io::{Read, Write},
-    net::TcpListener,
-};
+use std::{io::{Read, Write}, net::TcpListener};
 
 fn main() {
+    // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-
+    // Uncomment this block to pass the first stage
+    //
+    let listener: TcpListener = TcpListener::bind("127.0.0.1:6379").unwrap();
+    
     for stream in listener.incoming() {
         match stream {
-            Ok(mut stream) => {
+            Ok(mut _stream) => {
                 println!("accepted new connection");
-
                 let mut buffer = [0; 1024];
-
-                loop {
-                    let bytes_read = match stream.read(&mut buffer) {
-                        Ok(0) => break, // connection closed
-                        Ok(n) => n,
-                        Err(_) => break,
-                    };
-
-                    let input = String::from_utf8_lossy(&buffer[..bytes_read]);
-                    println!("received: {}", input);
-
-                    if input.contains("PING") {
-                        stream.write_all(b"+PONG\r\n").unwrap();
-                    }
-                }
+                _stream.read(&mut buffer).unwrap();
+                _stream.write_all(b"+PONG\r\n").unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
